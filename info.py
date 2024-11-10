6a9a1a2c9467c561ce214e926a48184742c8c2d3
Jihad1350 <technicalseekho249@gmail.com>
2024-11-10 13:41:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-SECONDDB_URI = environ.get('DATABASE_URI', "mongodb+srv://SourabhProfessor:SourabhProfessor@cluster0.a0bz4ht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SECONDDB_URI = environ.get('DATABASE_URI', "mongodb+srv://VJBOTHIIFYI:VJBOTHIIFYI@cluster0764776.t5xfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0764776")
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://VJBOTHIIFYI:VJBOTHIIFYI@cluster0764776.t5xfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0764776")
 DATABASE_NAME = environ.get('DATABASE_NAME', "VJBOTHIIFYI")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
